f96e915a3439f19cb01ed7617e4a6602946d25e3
Gottch7897 <gottschalkmartin7@gmail.com>
2025-09-07 19:09:02 -0300

    segundo commit

--- Proyecto/appBakan/src/app/pagina2/pagina2.page.ts
+++ Proyecto/appBakan/src/app/pagina2/pagina2.page.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { IonButton, IonAlert, IonCheckbox, IonDatetime } from '@ionic/angular/standalone';
-
+import {IonBadge, IonItem, IonLabel, IonList} from '@ionic/angular/standalone';
 @Component({
   selector: 'app-pagina2',
   templateUrl: './pagina2.page.html',
   styleUrls: ['./pagina2.page.scss'],
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonAlert, IonCheckbox, 
-    IonDatetime]
+    IonDatetime, IonBadge, IonItem, IonLabel, IonList]
 })
 export class Pagina2Page implements OnInit {
 

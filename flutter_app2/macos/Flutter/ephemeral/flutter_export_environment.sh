9cf50df3ba8f66c9ddc6cbcc5b4786a9531b8fce
Gottch7897 <gottschalkmartin7@gmail.com>
2025-10-20 13:05:13 -0300

    debounce fix lag

--- flutter_app2/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ flutter_app2/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\lenovo\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\lenovo\Desktop\PROYECTOS\TecnologiasMobiles\flutter_app2"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\gotts\Taller3\flutter_app2"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"

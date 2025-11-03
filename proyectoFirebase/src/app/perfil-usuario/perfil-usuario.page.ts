863468b866a5056ca0b645cc4c3e16649d24fe12
Gottch7897 <gottschalkmartin7@gmail.com>
2025-11-03 12:18:12 -0300

    Agregado perfil usuario y scss
    Falta:
    -Agregar breadcrumbs y navegacion en las otras paginas
    -Corregir diseño (lineas negras)

--- proyectoFirebase/src/app/perfil-usuario/perfil-usuario.page.ts
+++ proyectoFirebase/src/app/perfil-usuario/perfil-usuario.page.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,
+  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle,
+  IonCardTitle, IonButton, IonNote, IonLabel, IonItem, IonList, IonIcon, IonAvatar
+ } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-perfil-usuario',
   templateUrl: './perfil-usuario.page.html',
   styleUrls: ['./perfil-usuario.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    IonCard, IonCardContent, IonCardHeader, IonCardSubtitle,
+  IonCardTitle, IonButton, IonNote, IonLabel, IonItem, IonList, IonIcon, IonAvatar
+  ]
 })
 export class PerfilUsuarioPage implements OnInit {
 
